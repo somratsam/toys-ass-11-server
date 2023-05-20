6032c1886646b040645900974e822a34d7850090
somratsam <somratsam2@gmail.com>
2023-05-21 01:43:46 +0600

    create add toy to get data

--- index.js
+++ index.js
@@ -3,10 +3,10 @@ const cors = require('cors');
 const { MongoClient, ServerApiVersion } = require('mongodb');
 require('dotenv').config()
 const app = express()
-const port = process.env.port || 5000;
+const port = process.env.PORT || 5000;
 
 app.use(cors())
-app.use(express.json)
+app.use(express.json())
 
 
 console.log(process.env.DB_PASS);
@@ -28,12 +28,28 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+const toysLocation = client.db('toyMarketplace').collection('toys')
+const toyAddCollection = client.db('toyMarketplace').collection('addToy')
+app.get('/toys', async (req, res) => {
+  const cursor = toysLocation.find()
+  const result = await cursor.toArray()
+  res.send(result)
+})
+
+ app.post('/addToy', async (req, res) => {
+  const addToy = req.body
+  console.log(addToy);
+  const result = await toyAddCollection.insertOne(addToy)
+  res.send(result)
+ })
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
@@ -48,3 +64,9 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
     console.log(`toy server running on port, ${port}`);
 })
+
+
+
+
+
+
